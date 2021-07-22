f3bb5b6f904b70cc637cd69155b35e8e9f6267d3
José Victor Santos Vieira <criolo444@gmail.com>
2021-07-22 03:55:53 -0300

    Multiplos de três

--- src/exercicios/MultiplosDeTresAteCem.java
+++ src/exercicios/MultiplosDeTresAteCem.java
@@ -1,6 +1,6 @@
 package exercicios;
 
-class MultiplosDeTresAteCem {
+class MultiplosDeTresAteCem  {
 	public static void main(String[] args) {
 		for (int i = 3; i < 100; i += 3) {
 			System.out.println(i);
