bdb27c51075cc25eb046618fc815afa25a1f2eca
José Victor Santos Vieira <criolo444@gmail.com>
2021-07-22 03:53:49 -0300

    Teste de condicionais

--- src/exercicios/TestaCondicional2.java
+++ src/exercicios/TestaCondicional2.java
@@ -1,6 +1,6 @@
 package exercicios;
 
-public class TestaCondicional2 {
+public class TestaCondicional2  {
 
 	public static void main(String[] args) {
 		System.out.println("Teste de condicionais");
